0f3217e1365bbdad4662fb62173e414460e3c16d
ZhanPw <zhanpengwei@e.gzhu.edu.cn>
2018-09-09 17:56:44 +0800

    Python code
    Python code

---
+++ Python/#443_String Compression.py
@@ -0,0 +1,23 @@
+class Solution:
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        if len(chars) == 0 or len(chars) == 1:
+            return len(chars)
+        i = 0
+        while i < len(chars)-1:
+            count = 1
+            while i < len(chars)-1 and chars[i] == chars[i+1]:
+                count += 1
+                del chars[i]
+            if count > 1:
+                count = str(count)
+                n = len(count)
+                for j in range(n):
+                    chars.insert(i+1+j, count[j])
+                i += n+1
+            else:
+                i += 1
+        return len(chars)
